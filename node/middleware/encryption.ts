954ea6dbb31c5c3ae6dd68895ca2b05168faa043
andreeacalinvtex <93598087+andreeacalinvtex@users.noreply.github.com>
2022-01-06 10:28:22 +0200

    Create encryption.ts

---
+++ node/middleware/encryption.ts
@@ -0,0 +1,17 @@
+export async function encryption(ctx: Context, next: () => Promise<any>) {
+  const {
+    clients: { encryption}
+  } = ctx
+
+  try {
+    const data = encryption.getEncryption(ctx.request.query.plaintext, ctx.request.query.key)    
+    console.log('data: ', data);
+    ctx.status = 201
+    ctx.body = data
+  } catch (error) {
+    ctx.status = 502;
+    console.log(JSON.stringify(error));
+  }
+
+  await next()
+}
